5fda6a0c3e93819c9f3449aa70d571470f3a549d
bryan <bryanibit@gmail.com>
2021-01-09 23:25:17 +0800

    replace remove with erase O(n) to O(1)

--- LRU.cpp
+++ LRU.cpp
@@ -1,9 +1,7 @@
-#include <iostream> // for test in main
 #include <string> // for test in main
 #include <unordered_map> // for linked hashmap
 #include <list> // for linked hashmap
 using namespace std;
-
 template<typename T, typename S>
 struct Node {
 	T key;
@@ -11,33 +9,35 @@ struct Node {
 	Node() :key(T()), val(S()) {}
 	Node(T k, S v) :key(k), val(v) {}
 	// for list func remove
-	bool operator==(Node<T,S> nd) const { return nd.key == key; }
+	bool operator==(Node<T, S> nd) const { return nd.key == key; }
 };
-
 template<typename T, typename S>
 class LRUCache {
 private:
 	int capacity;
 	list < Node<T, S> > cache;
-	unordered_map<T, Node<T, S> > map;
+	unordered_map<T, typename list < Node<T, S> >::iterator > map;
 	void MakeRecent(T key) {
-		auto node = map[key];
-		cache.remove(node);
+		auto iter = map[key];
+		auto node = *iter;
+		cache.erase(iter);
 		cache.push_back(node);
+		map[key] = prev(cache.end());
 	}
 	void AddRecent(T key, S val) {
 		cache.emplace_back(key, val);
-		map.emplace(key, Node<T, S>(key, val));
+		auto iter_last = std::prev(cache.end());
+		map.emplace(key, iter_last);
 	}
 	void DeleteKey(T key) {
-		auto node = map[key];
-		cache.remove(node);
+		auto iter = map[key];
+		cache.erase(iter);
 		map.erase(key);
 	}
 	void DeleteLeastRecent() {
-		auto pa = cache.front();
+		auto node = cache.front();
 		cache.pop_front();
-		map.erase(pa.key);
+		map.erase(node.key);
 	}
 public:
 	LRUCache(int cap) :capacity(cap) {}
@@ -45,7 +45,8 @@ public:
 		if (map.find(key) == map.end())
 			return S();
 		MakeRecent(key);
-		return map[key].val;
+		auto iter = map[key];
+		return iter->val;
 	}
 	void put(T key, S val) {
 		if (map.find(key) != map.end()) {
@@ -58,6 +59,7 @@ public:
 	}
 };
 
+// for test as follow
 class A { // for test user-defined type
 public:
 	A() = default;
@@ -71,8 +73,13 @@ int main() {
 	lru.put(2, "3");
 	lru.put(3, "4");
 	lru.put(4, "5");
-	std::cout << lru.get(2) << std::endl;
-	std::cout << lru.get(1) << std::endl;
+	auto l1 = lru.get(1);
+	auto l2 = lru.get(2);
+	auto l3 = lru.get(3);
+	lru.put(9, "10");
+	lru.put(10, "11");
+	auto l9 = lru.get(9);
+
 	LRUCache<int, A> lru_a(2);
 	lru_a.put(1, A(3, 5));
 	lru_a.put(2, A(4, 5));
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## What is the Project
 
 The project is used for building linked hashmap with C++. LRU(Least Recently Used) is widely used in recent app, which is an example of linked hashmap.
+
+## What is the point
+
+`std::list::remove` time complexity is O(n) and `std::list::erase` is O(1). the latter's argument is iterator, instead of val itself, which is argument of the formmer. So the map's type is `unordered_map<T, list<Node<T, S>>::iterator>`.  
+When applying *erase* to list, then original pointer(iterator) is out of work. For example, in `MakeRecent`, we should update `map[key]` because original node is erased. All in all, we should make sure both is consistent.
